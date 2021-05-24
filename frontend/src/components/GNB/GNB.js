fb512ee4bc5adf167d4cc6786e7debaa0b1e2cc1
hyejin yang <hyejinyang@hyejinui-MacBookAir.local>
2021-05-24 13:12:34 +0900

    style : fix folder tree

---
+++ frontend/src/components/GNB/GNB.js
@@ -0,0 +1,12 @@
+// import PropTypes from 'prop-types';
+// // impt 치면 자동 생성
+
+// const GNB = () => {
+//   return <div></div>;
+// };
+
+// export default GNB;
+
+// // 코드 스니펫 사용 > rafc 등이 있음
+
+// setTask(tasks.filter(task) => task.id !== id)
