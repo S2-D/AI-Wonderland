0e037735241abe335c7644df08c225a71f560f80
hyejin yang <hyejinyang@hyejinui-MacBookAir.local>
2021-05-26 03:44:03 +0900

    feat : Creating Intro page

--- frontend/src/pages/Intro/Intro.js
+++ frontend/src/pages/Intro/Intro.js
@@ -1,89 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import IntroTalk from './IntroTalk';
 import logo from './Logo_example.jpeg';
 
-const talkData = {
-  rabbitSaying: [
-    {
-      id: 1,
-      text: 'Hello, You got lost again.',
-      show: false,
-    },
-    {
-      id: 2,
-      text: "Let me see... The way isn't the only thing you've lost.",
-      show: false,
-    },
-    {
-      id: 3,
-      text: "You've lost yourself too. The tea party in 2003, remember?",
-      show: false,
-    },
-    {
-      id: 4,
-      text:
-        "I'll take you to the vintage shop of the past. Go and find yourself.",
-      show: false,
-    },
-    {
-      id: 5,
-      text: 'But you know, you can only stay for a half hour in that era.',
-      show: false,
-    },
-  ],
-  userSaying: [
-    {
-      id: 1,
-      text: "Yes, I'm here to find the past again.",
-      show: false,
-    },
-    {
-      id: 2,
-      text: 'What do you mean? I got lost again?',
-      show: false,
-    },
-  ],
-};
-
-function Intro(talkData) {
-  // 화면 3가지로 나누기 : 1) 첫 진입 시(로그인/비로그인 나누어야 함), 2) 시작하기 버튼 클릭 시(토끼의 2가지 대사) 3) 선택 후 토끼의 대사
-
-  const [view, setView] = useState(-1);
-  const [rabbitTalk, setRabbitTalk] = useState(false);
-  const [userTalk, setUserTalk] = useState(false);
-
-  function firstView() {
+function Intro() {
+  return (
     // if (!user) - 차후 백엔드에서 유저 상태값 가져와서 관리?
-    return (
-      <>
-        <body>
-          <div className="container">
-            <div className="logo">
-              <img src={logo} />
-            </div>
-            <button className="btn" onClick={(e) => setView(0)}>
-              Click to Start
-            </button>
-          </div>
-        </body>
-      </>
-    );
-  }
-
-  function secondView() {
-    return (
-      <>
-        <h1>질문지(예)</h1>
-      </>
-    );
-  }
-
-  if (index < 0) {
-    return (
-      <>
-        <callFirstView />
-      </>
-    );
-  }
+    // 첫 진입 시(로그인/비로그인 나누어야 함) - key를 받아와야 하고, 새로고침을 해도 유지가 되는 storage에서 받아와야 - mock 서버 만들어서 확인
+    <>
+      <div className="container">
+        <div className="logo">
+          <img src={logo} />
+        </div>
+        <IntroTalk />
+      </div>
+    </>
+  );
 }
 
 export default Intro;
---
+++ frontend/src/pages/Intro/IntroTalk.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function IntroTalk() {
+  const rabbitSaying = [
+    {
+      id: 1,
+      text: "Let me see... * Well, there is another thing you've lost.",
+    },
+    {
+      id: 2,
+      text: "You've lost yourself too. * The tea party in 2003, remember?",
+    },
+    {
+      id: 3,
+      text:
+        "I'll take you to the vintage shop of the past. * Go and find yourself.",
+    },
+    {
+      id: 4,
+      text: 'But you know, you can only stay for a half hour in that era.',
+    },
+  ];
+
+  const [index, setIndex] = useState(0);
+  // 1) 유저가 1번을 선택 > popup 노출 > 로그인
+  // 2) 유저가 2번을 선택 > 토끼 대화 > popup 노출 > 메인
+
+  function CallUserTalk() {
+    return (
+      <>
+        <Link to="/main">
+          <button>Yes, I'm here to find the past again.</button>
+        </Link>
+
+        <button onClick={() => setIndex(3)}>
+          What do you mean? I got lost again?
+        </button>
+      </>
+    );
+  }
+
+  function CallRabbitTalk() {
+    return (
+      <>
+        {rabbitSaying.map((data, idx) => (
+          <button key={idx}>{data.text.split('* ')}</button>
+        ))}
+        {/* <button onClick={() => {}} {rabbitSaying.text}</button> */}
+      </>
+    );
+  }
+
+  if (index === 0) {
+    return <button onClick={() => setIndex(1)}>Click to start</button>;
+  } else if (index === 1) {
+    return (
+      <button onClick={() => setIndex(2)}>Hello, You got lost again.</button>
+    );
+  } else if (index === 2) {
+    return <CallUserTalk />;
+  } else if (index === 3) {
+    return <CallRabbitTalk />;
+  }
+}
+
+export default IntroTalk;
