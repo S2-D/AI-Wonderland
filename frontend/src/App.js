0cced201d3a1b85857f2f562110926efb69aa7c3
나윤이의 맥북에어 <nayun-lily@nayun-MacBook.local>
2021-05-27 00:55:33 +0900

    feat: added main page and Carousel component. 메인페이지 파일 추가와 캐러셀 추가하는데에 성공

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
+/* eslint-disable no-unused-vars */
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import MainPage from './components/views/MainPage/MainPage';
+import Auth from './hoc/auth';
 
 function App() {
-  const name = 'Brad';
   return (
-    <div className="App">
-      <h1>project start</h1>
-      <h2>Hello {name}</h2>
-    </div>
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/" component={Auth(LandingPage, null)} />
+          <Route exact path="/login" component={Auth(LoginPage, false)} />
+          <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/main" component={Auth(MainPage, null)} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/components/views/MainPage/MainPage.js
+++ frontend/src/components/views/MainPage/MainPage.js
@@ -7,7 +7,34 @@ import { Carousel } from 'antd';
 import { withRouter } from 'react-router-dom';
 
 function MainPage() {
-  return <h2>Hello, this is main page.</h2>;
+  const contentStyle = {
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+  };
+
+  return (
+    <>
+      <div className="container">
+        <Carousel autoplay>
+          <div>
+            <h3 style={contentStyle}>1</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>2</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>3</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>4</h3>
+          </div>
+        </Carousel>
+      </div>
+    </>
+  );
 }
 
 export default withRouter(MainPage);
