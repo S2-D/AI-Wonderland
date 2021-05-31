32dc3c753978c2bf14b30452c2d0c4d0e2fa6396
Ubuntu <azure@elice-kdt-ai-track-vm-ai-24.hqbktzrxcscu3ivrumcvgloqqd.syx.internal.cloudapp.net>
2021-05-31 08:56:12 +0000

    Update VM

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -158,10 +158,10 @@ USE_TZ = True
 ROOT_DIR = os.path.join(BASE_DIR)
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static') 
+# STATIC_DIR = os.path.join(BASE_DIR, 'static') 
 # STATICFILES_DIR = [ STATIC_DIR ]
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT = os.path.join('static')
+STATIC_ROOT = os.path.join('static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
