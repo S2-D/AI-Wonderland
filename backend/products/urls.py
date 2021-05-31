3b91e3dfaecc4f4fcf215a02ae20ff1a87eb244e
Ubuntu <azure@elice-kdt-ai-track-vm-ai-24.hqbktzrxcscu3ivrumcvgloqqd.syx.internal.cloudapp.net>
2021-05-31 09:04:29 +0000

    Update products/urls.py

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import url
 router = routers.DefaultRouter()
 router.register(r'categotylist', views.CategoryList)
 router.register(r'productlist', views.ProductList)
-router.register(r'productlist2', views.ProductList2)
+#router.register(r'productlist2', views.ProductList2)
 router.register(r'reviewlist', views.ReviewList)
 router.register(r'productTop4List', views.ProductTop4List)
 
