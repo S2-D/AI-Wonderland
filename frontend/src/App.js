1086f344436f72bcdb3c261aba7325f4e5ccd474
나윤이의 맥북에어 <nayun-lily@nayun-MacBook.local>
2021-06-01 22:30:41 +0900

    chore: changed Url.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,11 @@ import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
 import MainPage from './components/views/MainPage/MainPage';
-import GNB from './components/views/GNB/GNB';
 import Intro from './pages/Intro/Intro';
 import ProductList from './pages/Product/ProductList';
 
 import 'bootstrap/dist/css/bootstrap.css';
 // 최상위 컴포넌트에 적용해주면 일괄 적용 가능함
-
 
 function App() {
   return (
@@ -20,15 +18,16 @@ function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route exact path="/" component={LandingPage} />
+          {/* 로그인 페이지  연결 */}
           <Route exact path="/login" component={LoginPage} />
+          {/* 회원가입 페이지  연결 */}
           <Route exact path="/register" component={RegisterPage} />
+          {/* 메인 페이지  연결 */}
           <Route exact path="/main" component={MainPage} />
-          <Route exact path="/gnb" component={GNB} />
           {/* 인트로 페이지  연결 */}
-        <Route exact path="/" component={Intro} />
-        {/* 상품 리스트 페이지 연결  */}
-        <Route exact path="/productList" component={ProductList} />
+          <Route exact path="/" component={Intro} />
+          {/* 상품 리스트 페이지 연결  */}
+          <Route exact path="/productList" component={ProductList} />
         </Switch>
       </div>
     </Router>
