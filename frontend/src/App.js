5942a3c2716e4a7021c6e6ccfa05588f4d82c6ad
hyejin yang <hyejinyang@hyejinui-MacBookAir.local>
2021-06-02 20:34:02 +0900

    feat : Revise the folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,11 @@ import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
 import MainPage from './components/views/MainPage/MainPage';
-import Intro from './pages/Intro/Intro';
-import ProductList from './pages/Product/ProductList';
 
 import 'bootstrap/dist/css/bootstrap.css';
-import ProductDetail from './pages/Product/ProductDetail';
+import ProductList from './components/views/Product/ProductList';
+import ProductDetail from './components/views/Product/ProductDetail';
+import Intro from './components/views/Intro/Intro';
 // 최상위 컴포넌트에 적용해주면 일괄 적용 가능함
 
 function App() {
@@ -29,6 +29,7 @@ function App() {
           <Route exact path="/" component={Intro} />
           {/* 상품 리스트 페이지 연결  */}
           <Route exact path="/productList" component={ProductList} />
+          {/* 상품 상세 페이지 연결 */}
           <Route exact path="/productDetail" component={ProductDetail} />
         </Switch>
       </div>
