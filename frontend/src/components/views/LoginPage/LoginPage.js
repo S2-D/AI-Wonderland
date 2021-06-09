1a5cb220046c6f57a2ba2790d7ccc399a6cb664c
나윤이의 맥북에어 <nayun-lily@nayun-MacBook.local>
2021-06-10 00:19:50 +0900

    fix: login error fixed!

--- frontend/src/components/views/LoginPage/LoginPage.js
+++ frontend/src/components/views/LoginPage/LoginPage.js
@@ -17,14 +17,16 @@ function LoginPage({ setLoginUserId }) {
 
   const loginFormPost = (data) => {
     axios.post(baseUrl + '/member/signIn/', data).then((response) => {
-      if (response.data.status === 'success') {
+      if (response.data.message === '로그인에 성공하였습니다.') {
         console.log(response.data);
         const access_token = response.data.token;
         localStorage.setItem('access_token', access_token);
         // setLoginUserId(response.data.token);
         history.push('/main');
       } else {
-        alert(response.data.error);
+        alert(
+          "Please check the input values! If you don't have an Id, please register!"
+        );
         document.getElementById('password').focus();
       }
     });
@@ -40,40 +42,61 @@ function LoginPage({ setLoginUserId }) {
   };
 
   return (
-    <form className="loginForm" onSubmit={handleSubmit(onSubmit)}>
-      {/* Email */}
-      <label>Email</label>
-      <input
-        type="email"
-        id="email"
-        name="email"
-        placeholder="email"
-        defaultValue=""
-        {...register('email', { required: true })}
-      />
-      {errors.email && <p>Please enter your email.</p>}
+    <div className="loginContainer">
+      <div className="loginInfoBox">
+        <div className="grid grid-row-5">
+          <form className="loginForm" onSubmit={handleSubmit(onSubmit)}>
+            <div className="row-span-1 p-2">
+              <img
+                src="./images/new_logo_blue.png"
+                className="loginLogo"
+                style={{ backgroundColor: 'white' }}
+              />
+              <span>AI WONDERLAND</span>
+            </div>
+            <div className="row-span-2 p-2">
+              {/* Email */}
+              <label>Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="email"
+                defaultValue=""
+                {...register('email', { required: true })}
+              />
+              {errors.email && <p>Please enter your email.</p>}
+            </div>
 
-      {/* 비밀번호 */}
-      <label>비밀번호</label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        placeholder="password"
-        {...register('password', { required: true, maxLength: 10 })}
-      />
-      {errors.password && <p>Please enter your password.</p>}
-
-      {/* login button */}
-      <input className="btnLogin" type="submit" value="Login" />
-      <p>If you don't have ID, click the button "Sign Up"!</p>
-      <input
-        className="btnSignup"
-        type="button"
-        value="Sign Up"
-        onClick={onClickSignup}
-      />
-    </form>
+            <div className="row-span-3 p-2">
+              {/* 비밀번호 */}
+              <label>비밀번호</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="password"
+                {...register('password', { required: true, maxLength: 10 })}
+              />
+              {errors.password && <p>Please enter your password.</p>}
+            </div>
+            <div className="row-span-4 p-2">
+              {/* login button */}
+              <input className="btnLogin" type="submit" value="Login" />
+            </div>
+            <div className="row-span-5 p-2">
+              <p>If you don't have ID, click the button "Sign Up"!</p>
+              <input
+                className="btnSignup"
+                type="button"
+                value="Sign Up"
+                onClick={onClickSignup}
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
