b852c0d04cc244540c504f7cc272f237b29699da
hyejin yang <hyejinyang@hyejinui-MacBookAir.local>
2021-06-11 03:27:07 +0900

    feat : Add product list's css

--- frontend/src/components/views/Product/ProductListCard.js
+++ frontend/src/components/views/Product/ProductListCard.js
@@ -1,19 +1,30 @@
 import React from 'react';
 
+import styledProductListCard from './styledProductListCard.css';
+
 export default function ProductListCard() {
   return (
-    <div className="flex flex-col justify-center gap-1">
-      <img src="/images/intro/rabbit02_nerd.png"></img>
-      <p>$ 1,00098</p>
-      <p>Product Name</p>
-      <i
-        className="fas fa-shopping-cart"
-        style={{
-          display: 'flex',
-          justifyContent: 'flex-end',
-          color: 'grey',
-        }}
-      ></i>
+    <div className="product-list-card">
+      <img
+        className="product-list-card-img"
+        src="/images/intro/rabbit02_nerd.png"
+      ></img>
+      <section>
+        <img
+          className="product-list-card-p_price-coin"
+          src="/images/icon_img/coin_fixed.png"
+        ></img>
+        <div className="product-list-card-p_price">$ 1,00098</div>
+      </section>
+      <section className="product-list-card-p_name">
+        Product Name It's really really wrong
+      </section>
+      <section className="flex justify-end">
+        <img
+          src="/images/icon_img/product_cart.png"
+          className="product-list-card-icon"
+        ></img>
+      </section>
     </div>
   );
 }
